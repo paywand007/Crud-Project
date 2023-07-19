dfb4b158c81ae6678c9b540633770acb7e8b7d85
paywand007 <paywandmohammed007@gmail.com>
2023-07-19 16:26:04 +0300

    Adding multi languages to error messages

--- src/plugins/veeValidate.ts
+++ src/plugins/veeValidate.ts
@@ -1,3 +1,10 @@
 import { defineRule } from "vee-validate";
-import { required } from "@vee-validate/rules";
-defineRule("required", required);
+import { useI18n } from "vue-i18n";
+import i18n from "../i18n.ts";
+
+defineRule("required", (value) => {
+  if (!value || !value.length) {
+    return i18n.global.t("require");
+  }
+  return true;
+});
