aa788f7764cb1594a57205549b8e799cd590d05c
paywand007 <paywandmohammed007@gmail.com>
2023-07-19 11:33:58 +0300

    ref search functionality

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -1,31 +1,37 @@
 <template>
-  <v-data-table :headers="headers" :items="data" :search="searchTerm">
-    <template v-slot:top>
-      <v-toolbar class="bg-white my-5">
-        <v-toolbar-title
-          ><p class="text-h3 font-weight-bold">
-            {{ $t("staff") }}
-          </p></v-toolbar-title
-        >
-
-        <v-text-field
-          v-model="searchRes"
-          :label="t('search')"
-          class="mt-5 bg-white"
-          clearable
-          @keyup.enter="searchFn"
-        >
-        </v-text-field>
-
-        <v-spacer></v-spacer>
-        <v-btn color="pink-darken-4" variant="elevated" size="large" class="">
-          <RouterLink to="/addEdit" class="text-decoration-none text-white">
-            {{ $t("addStaff") }}
-            <v-icon> mdi-account-plus</v-icon>
-          </RouterLink>
-        </v-btn>
-      </v-toolbar>
-    </template>
+  <v-row class="mt-5">
+    <v-col cols="12" sm="3" md="3">
+      <p class="text-h3 font-weight-bold">
+        {{ $t("staff") }}
+      </p>
+    </v-col>
+    <v-col cols="12" sm="4" md="4">
+      <v-text-field
+        v-model="searchRes"
+        :label="t('search')"
+        class="bg-white"
+        clearable
+        @click:clear="refreshData"
+        @keydown.enter="performSearch"
+      >
+      </v-text-field>
+    </v-col>
+    <v-col cols="12" sm="5" md="5" class="align-content-center text-center">
+      <v-btn
+        color="pink-darken-4"
+        variant="elevated"
+        min-width="100px"
+        size="large"
+        class=""
+      >
+        <RouterLink to="/addEdit" class="text-decoration-none text-white">
+          {{ $t("addStaff") }}
+          <v-icon> mdi-account-plus</v-icon>
+        </RouterLink>
+      </v-btn>
+    </v-col>
+  </v-row>
+  <v-data-table :headers="headers" :items="data">
     <template v-slot:item="{ item }">
       <tr>
         <td>{{ item.selectable.firstName }} {{ item.selectable.lastName }}</td>
@@ -55,7 +61,7 @@
               mdi-delete
             </v-icon>
 
-            <v-dialog v-model="dialog" max-width="500px" persistent>
+            <v-dialog v-model="dialog" max-width="500px" :persistent="true">
               <v-card>
                 <v-card-title class="text-h5">{{
                   t("sueDelete")
@@ -100,7 +106,7 @@
 <script lang="ts" setup>
 import apiData from "./apiData.ts";
 import { RouterLink, useRouter, useRoute } from "vue-router";
-import { onMounted, ref, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 const router = useRouter();
 
@@ -110,7 +116,7 @@ const dialog = ref(false);
 const { id } = route.params.id;
 const searchTerm = ref("");
 
-const searchRes = ref(" ");
+const searchRes = ref("");
 watch(searchTerm, () => {
   if (searchTerm.value === searchRes.value) {
     return searchTerm.value;
@@ -118,10 +124,6 @@ watch(searchTerm, () => {
     return searchTerm.value;
   }
 });
-const searchFn = () => {
-  searchTerm.value = searchRes.value;
-  router.push({ path: "/ ", query: { search: searchRes.value } });
-};
 
 const dialogDelete = (idItem) => {
   dialog.value = true;
@@ -137,38 +139,42 @@ const headers = ref([
 ]);
 const data = ref([]);
 const fetchDta = async () => {
-  await apiData
-    .get("/posts", {
-      params: {
-        search: searchRes.value,
-      },
-    })
-    .then((res) => (data.value = res.data));
+  await apiData.get("/posts").then((res) => (data.value = res.data));
 };
 const deleteData = async () => {
-  try {
-    await apiData
-      .delete(`/posts/${route.params.id}`)
-      .then(() => {
-        dialog.value = false;
-        console.log(`Deleted post with ID ${route.params.id}`);
-        router.push("/");
-        fetchDta();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } catch (err) {
-    console.log(err);
-  }
+  await apiData
+    .delete(`/posts/${route.params.id}`)
+    .then(() => {
+      dialog.value = false;
 
-  // await apiData.delete(`/posts/${idD}`).then(() => {
-  //   dialog.value = !dialog.value;
-  //   console.log(`Deleted post with ID ${idD}`);
-  //   fetchDta();
-  // });
+      router.push("/");
+      fetchDta();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 onMounted(() => {
   fetchDta();
 });
+// const performSearch = () => {
+//   apiData
+//     .get(`/staff?q=${searchRes.value}`)
+//     .then((res) => (data.value = res.data));
+// };
+const performSearch = async () => {
+  try {
+    await apiData
+      .get(`/posts?q=${searchRes.value as string}`)
+      .then((res) => (data.value = res.data));
+    // const response = await apiData.get(`/posts`);
+    // data.value = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+const refreshData = () => {
+  searchRes.value = "";
+  fetchDta();
+};
 </script>
